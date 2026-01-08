ea09051fa411791d6261af39827adef053196b1f
Marton A. Varga <martonaronvarga@gmail.com>
2026-01-08 15:29:18 +0100

    debug: set up aerc and gmail bridge
    - Updated `home/programs/email/aerc.nix` for Gmail bridge configuration.
    - Updated `modules/agenix.nix` to manage secrets.
    - Added encrypted secrets: `aerc_client_id.age`, `aerc_client_secret.age`, and `aerc_refresh_token.age`.
    - Updated `secrets/secrets.nix` to include new secrets.
    - Modified `secrets/usu.age` as part of secret management updates.
    
    This commit enables OAuth-based authentication for aerc while keeping all credentials encrypted and declaratively managed via agenix.
    A hacky solution is implemented to read secrets after activation and create ~/.config/aerc/accounts.conf, because aerc only supports static URIs.

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -10,6 +10,22 @@
       owner = "usu";
       mode = "600";
     };
+    aerc-client-id = {
+      file = toString ../secrets/aerc_client_id.age;
+      owner = "usu";
+      mode = "0400";
+      path = "/run/agenix/aerc-client-id";
+    };
+    aerc-client-secret = {
+      file = toString ../secrets/aerc_client_secret.age;
+      owner = "usu";
+      mode = "0400";
+      path = "/run/agenix/aerc-client-secret";
+    };
+    aerc-refresh-token = {
+      file = toString ../secrets/aerc_refresh_token.age;
+      owner = "usu";
+    };
   };
-  age.identityPaths = [ "/persist/home/usu/.ssh/id_ed25519" ];
+  age.identityPaths = ["/persist/home/usu/.ssh/id_ed25519"];
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
-  usu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3xygPFeJRmLkyiV0P/vak54Wh7ggq9B6HanmUa137A usu@shade
-";
+  usu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3xygPFeJRmLkyiV0P/vak54Wh7ggq9B6HanmUa137A usu@shade";
 in {
   "usu.age".publicKeys = [usu];
+  "aerc_client_id.age".publicKeys = [usu];
+  "aerc_client_secret.age".publicKeys = [usu];
+  "aerc_refresh_token.age".publicKeys = [usu];
 }
